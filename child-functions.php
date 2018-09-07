456d3e6a1dd49cd6898407f69a33afbb34fb8926
rcagle4 <ryan.d.cagle@gmail.com>
2018-09-07 12:04:42 -0400

    Update child-functions.php

--- child-functions.php
+++ child-functions.php
@@ -1,34 +1,46 @@
-add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+add_action( 'wp_enqueue_scripts', 'ido_enqueue_parent_styles' );
 
 function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/bkup.css' );
 }
-add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+
+add_action( 'wp_enqueue_scripts', 'ido_enqueue_parent_styles' );
 
 function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/fancybox.css' );
 }
-add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+
+add_action( 'wp_enqueue_scripts', 'ido_enqueue_parent_styles' );
 
 function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/flexslider.css' );
 }
 
+add_action( 'wp_enqueue_scripts', 'ido_enqueue_parent_styles' );
+
 function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/foundation-responsive.css' );
 }
+
+add_action( 'wp_enqueue_scripts', 'ido_enqueue_parent_styles' );
 
 function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/ie7-style.css' );
 }
 
+add_action( 'wp_enqueue_scripts', 'ido_enqueue_parent_styles' );
+
 function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/jquery.fancybox-thumbs.css' );
 }
 
+add_action( 'wp_enqueue_scripts', 'ido_enqueue_parent_styles' );
+
 function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/nivoslider.css' );
 }
+
+add_action( 'wp_enqueue_scripts', 'ido_enqueue_parent_styles' );
 
 function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/superfish.css' );
